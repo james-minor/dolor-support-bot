17904f07b9a3b6257c8641258586ca55675b0aa5
James Minor <james.minor.business@gmail.com>
2025-03-20 15:23:37 -0500

    feat: added logging and ephemeral responses

--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
-# TODO: ephemeral responses
-# TODO: logging
 # TODO: prevent multiple registrations (utilize a simple sqlite db).
+
+# TODO: give user role to registered user, role allows for viewing stuff.
+# TODO: have default text placed into support channel.
 
 import discord
 import os
@@ -22,38 +23,45 @@ async def on_ready():
 
 @bot.command(description="Registers the user to the support system.")
 async def register(ctx: discord.ApplicationContext, name: discord.SlashCommandOptionType.string):
-    # Registering the member.
+    # Creating member object.
+    member: discord.Member = ctx.guild.get_member(ctx.author.id)
+
+    # Setting member nickname.
     try:
-        # Setting member nickname.
-        member: discord.Member = ctx.guild.get_member(ctx.author.id)
-        # TODO: uncomment this, doing it for testing
-        #await member.edit(nick=name)
-
-        # Creating support channel category (if it did not already exist).
-        category = discord.utils.get(ctx.guild.categories, name="support")
-        if not category:
-            category = await ctx.guild.create_category("support")
-
-        # Creating the support role (if it did not already exist).
-        if not discord.utils.get(ctx.guild.roles, name="Support Staff"):
-            support_role: discord.Role = await ctx.guild.create_role(name="Support Staff")
-        else:
-            support_role: discord.Role = discord.utils.get(ctx.guild.roles, name="Support Staff")
-
-        # Creating member support channel.
-        overwrites = {
-            ctx.guild.default_role: discord.PermissionOverwrite(read_messages=False),
-            support_role: discord.PermissionOverwrite(read_messages=True),
-            member: discord.PermissionOverwrite(read_messages=True),
-        }
-
-        new_channel_name: str = "-".join(name.lower().split()) + "-support"
-        new_channel: discord.TextChannel = await ctx.guild.create_text_channel(name=new_channel_name, category=category, overwrites=overwrites)
-
-
-        await ctx.respond("Successfully registered you to the support system!")
+        await member.edit(nick=name)
     except BaseException:
-        await ctx.respond("Could not register you to the support system (does not work for admins).")
+        await ctx.respond("Could not change your nickname (does not work for admins).", ephemeral=True)
+
+    # Creating support channel category (if it did not already exist).
+    category = discord.utils.get(ctx.guild.categories, name="support")
+    if not category:
+        category = await ctx.guild.create_category("support")
+
+    # Creating the support role (if it did not already exist).
+    if not discord.utils.get(ctx.guild.roles, name="Support Staff"):
+        support_role: discord.Role = await ctx.guild.create_role(name="Support Staff")
+    else:
+        support_role: discord.Role = discord.utils.get(ctx.guild.roles, name="Support Staff")
+
+    # Creating role overrides to make member support channel private.
+    overwrites = {
+        ctx.guild.default_role: discord.PermissionOverwrite(read_messages=False),
+        support_role: discord.PermissionOverwrite(read_messages=True),
+        member: discord.PermissionOverwrite(read_messages=True),
+    }
+
+    # Creating member support channel.
+    new_channel_name: str = "-".join(name.lower().split()) + "-support"
+    new_channel: discord.TextChannel = await ctx.guild.create_text_channel(
+        name=new_channel_name,
+        category=category,
+        overwrites=overwrites
+    )
+    print(f"Successfully created support channel '{new_channel_name}'.")
+
+    # End-user response.
+    await ctx.respond("Successfully registered you to the support system!", ephemeral=True)
+    print(f"Successfully registered user '{ctx.user}' to the support system.")
 
 
 bot.run(str(os.getenv("DISCORD_API_TOKEN")))
